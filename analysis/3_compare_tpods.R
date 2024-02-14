1169d7f23fa7bde384642d4499ece2b9168784ac
jessica-ewald <33487514+jessica-ewald@users.noreply.github.com>
2024-02-14 15:44:43 -0500

    refactor code

---
+++ analysis/3_compare_tpods.R
@@ -0,0 +1,10 @@
+#### Compute differences in tPOD distributions across conditions ####
+
+# Metrics: 
+# KS p-value for tPOD dists between conditions, for each of the 11 chemicals (paired?)
+# Paired difference of tPOD dist mean (1 test for all 11 chemicals) between conditions
+# Paired difference of tPOD dist variance (1 test for all 11 chemicals) between conditions
+
+# Across tPOD types for the same dataset
+
+# Across datasets for the same tpod type
